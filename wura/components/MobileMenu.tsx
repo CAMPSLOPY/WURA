1fe7423fee90689000ecae128cbb0c102f872b86
CAMPSLOPY <shobowaletola@gmail.com>
2024-04-03 20:20:37 +0200

    Menu components on mobile screen

--- wura/components/MobileMenu.tsx
+++ wura/components/MobileMenu.tsx
@@ -18,6 +18,21 @@ if(!visible){
         <div className=" px-3 text-center text-white hover:underline">
             Home
         </div>
+        <div className=" px-3 text-center text-white hover:underline">
+            Series
+        </div>
+        <div className=" px-3 text-center text-white hover:underline">
+            Films
+        </div>
+        <div className=" px-3 text-center text-white hover:underline">
+            New & Popular
+        </div>
+        <div className=" px-3 text-center text-white hover:underline">
+           My List
+        </div>
+        <div className=" px-3 text-center text-white hover:underline">
+            Search  by Languages
+        </div>
       </div>
     </div>
   )
