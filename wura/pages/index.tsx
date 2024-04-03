258b6a8373d0d6641543645803531174387cd36c
CAMPSLOPY <shobowaletola@gmail.com>
2024-04-03 20:17:20 +0200

    navbar added

--- wura/pages/index.tsx
+++ wura/pages/index.tsx
@@ -25,6 +25,32 @@ export default function Home() {
  return (
     <>
      <Navbar/>
+     <div className="bg-gray-500">
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+     </div>
     </>
   );
 }
