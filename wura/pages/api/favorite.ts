8d8a2fb675bb16982e08ff278e2673e6846abf5b
CAMPSLOPY <shobowaletola@gmail.com>
2024-04-05 01:48:59 +0200

    favorite

---
+++ wura/pages/api/favorite.ts
@@ -0,0 +1,67 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { without } from "lodash";
+import prismadb from "@/lib/prismadb";
+import serverAuth from "@/lib/Serverauth";
+// use to add and remove our favorite IDS
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        if (req.method === "POST") {
+            const { currentUser } = await serverAuth(req);
+
+            // Parse the incoming JSON body
+            const { movieId } = JSON.parse(req.body);
+
+            const existingMovie = await prismadb.movie.findUnique({
+                where: { id: movieId },
+            });
+
+            if (!existingMovie) {
+                throw new Error('Invalid ID');
+            }
+
+            const user = await prismadb.user.update({
+                where: {
+                    email: currentUser.email || '',
+                },
+                data: {
+                    favoriteIds: {
+                        push: movieId,
+                    }
+                }
+            });
+
+            return res.status(200).json(user);
+        }
+
+        if (req.method === 'DELETE') {
+            const { currentUser } = await serverAuth(req);
+            const { movieId } = JSON.parse(req.body);
+
+            const existingMovie = await prismadb.movie.findUnique({
+                where: { id: movieId }
+            });
+
+            if (!existingMovie) {
+                throw new Error('Invalid ID');
+            }
+
+            // Ensure `without` is imported correctly
+            const updateFavoriteIds = without(currentUser.favoriteIds, movieId);
+
+            const updatedUser = await prismadb.user.update({
+                where: {
+                    email: currentUser.email || '',
+                },
+                data: {
+                    favoriteIds: updateFavoriteIds,
+                }
+            });
+
+            return res.status(200).json(updatedUser);
+        }
+        return res.status(405).end();
+    } catch (error) {
+        console.error(error);
+        return res.status(400).end();
+    }
+}
