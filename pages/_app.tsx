e00d326f49eff55915388747f5565a23245219d9
CAMPSLOPY <54154401+CAMPSLOPY@users.noreply.github.com>
2024-05-08 19:40:33 +0000

    final comit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,16 @@
 import "@/styles/globals.css";
+import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps, session}: AppProps) {
-  return <SessionProvider
-  session={session}>
+interface Props extends AppProps {
+  session: Session;
+}
 
-<Component {...pageProps} />
-
-  </SessionProvider>
-  
-  
+export default function App({ Component, pageProps, session }: Props) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
